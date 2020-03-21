6107ef26cf4001bc535af8406fce4edf8ca56d94
AlexBogdanov <alex.e.bogdanov@gmail.com>
2020-03-21 14:50:38 +0200

    Solid lab added

---
+++ JavaOOP/06SOLID/Lab/p03_LiskovSubstitution/p01_Square/Main.java
@@ -0,0 +1,9 @@
+package solidLab.p03_LiskovSubstitution.p01_Square;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Rectangle();
+
+        System.out.println(shape.getArea());
+    }
+}
